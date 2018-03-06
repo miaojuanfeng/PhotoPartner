f36954465ce21d92101a85cd5fd20497f0ed955e
miaojuanfeng <miaojuanfeng@qq.com>
2018-03-06 10:48:27 +0800

    Initial Commit

---
+++ PhotoPartner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhotoPartner
+//
+//  Created by USER on 6/3/2018.
+//  Copyright © 2018 MJF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
